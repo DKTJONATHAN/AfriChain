8f1043d7e601f0d4c8bf563f4eeed4d6ad09c0ea
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-05-23 21:29:52 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5IocPCGZ7M8R2FmEnR3W7ESZrjE2-zsE",
+    authDomain: "africhainkenya.firebaseapp.com",
+    projectId: "africhainkenya",
+    storageBucket: "africhainkenya.firebasestorage.app",
+    messagingSenderId: "769906450985",
+    appId: "1:769906450985:web:36f46a9c54fb4317abb238",
+    measurementId: "G-RNZ05CXR9M"
+};
